6c83374ff5c29aaa2310d87f37c69e674c051f6b
DmitrySaf <dmitry.study@yandex.ru>
2022-11-10 20:26:45 +0300

    moved initialization to app folder #4

---
+++ src/app/sliderInit.ts
@@ -0,0 +1,14 @@
+import Model from './mvp/model/model';
+import Presenter from './mvp/presenter/presenter';
+import View from './mvp/view/view';
+import { IDefinedOptions, defaultOptions, IUndefinedOptions } from './options';
+
+$.fn.slider = function Slider(options: IUndefinedOptions) {
+  const settings: IDefinedOptions = $.extend({ ...defaultOptions }, options);
+  const model = new Model(settings);
+  const view = new View(this, model.options);
+  const presenter = new Presenter(view, model);
+
+  $(this).data('slider', presenter);
+  return presenter;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-import Model from './app/mvp/model/model';
-import Presenter from './app/mvp/presenter/presenter';
-import View from './app/mvp/view/view';
-import { IDefinedOptions, defaultOptions, IUndefinedOptions } from './app/options';
+import './app/sliderInit';
 import './demo/demo';
 import './demo/panel/panel';
 
 import './app/styles/slider.sass';
 import './assets/sass/styles.sass';
 import './demo/panel/panel.sass';
-
-$.fn.slider = function Slider(options: IUndefinedOptions) {
-  const settings: IDefinedOptions = $.extend({ ...defaultOptions }, options);
-  const model = new Model(settings);
-  const view = new View(this, model.options);
-  const presenter = new Presenter(view, model);
-
-  $(this).data('slider', presenter);
-  return presenter;
-};
