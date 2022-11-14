769ce6b817f9b20e0bc3251eced94414edb0beee
DmitrySaf <dmitry.study@yandex.ru>
2022-11-14 13:20:14 +0300

    updated tests for model #3

--- src/app/mvp/Model/__tests__/model.spec.ts
+++ src/app/mvp/Model/__tests__/model.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Model from '../Model';
 import { defaultOptions } from '../../../options';
 
@@ -5,6 +7,7 @@ const mockCoords = { from: 2001, to: 6000 };
 
 describe('Model', () => {
   const model = new Model(defaultOptions);
+  jest.spyOn(model, 'updateModelOptions');
 
   afterEach(() => {
     model.updateModelOptions(defaultOptions, mockCoords);
@@ -13,102 +16,90 @@ describe('Model', () => {
   it('changes value types where needed', () => {
     model.updateModelOptions({
       ...defaultOptions,
-      min: '123',
-      max: undefined,
-      from: null,
-      to: 'asd',
-      step: '-123',
-      vertical: 123,
-      double: null,
-      scale: '123', 
-      scaleFrequency: 'undefined',
-      valuesDisplay: 'null'
+      min: '123' as any,
+      max: undefined as any,
+      from: null as any,
+      to: 'asd' as any,
+      step: '-123' as any,
+      vertical: 123 as any,
+      double: null as any,
+      scale: '123' as any,
+      scaleFrequency: 'undefined' as any,
+      valuesDisplay: 'null' as any
     }, mockCoords);
 
     expect(model.options.min).toEqual(defaultOptions.min);
-    expect(model.options.vertical).toBeFalsy();
+    expect(model.options.max).toEqual(defaultOptions.max);
+    expect(model.options.from).toEqual(defaultOptions.min);
+    expect(model.options.to).toEqual(defaultOptions.to);
+    expect(model.options.step).toEqual(defaultOptions.step);
+    expect(model.options.vertical).toEqual(defaultOptions.vertical);
+    expect(model.options.double).toEqual(defaultOptions.double);
+    expect(model.options.scale).toEqual(defaultOptions.scale);
+    expect(model.options.scaleFrequency).toEqual(defaultOptions.scaleFrequency);
+    expect(model.options.valuesDisplay).toEqual(defaultOptions.valuesDisplay);
   });
 
   it('changes "max" value if "min" value is greater than "max" value', () => {
-    const options = { ...recievedOptions, min: 12000 };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, min: 12000, max: 11000 }, mockCoords);
 
     expect(model.options.max).toEqual(model.options.min + 1);
   });
 
   it('changes "step" value if "min" value is greater than "max" value', () => {
-    const options = { ...recievedOptions, min: 12000 };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, min: 12000, max: 11000 }, mockCoords);
 
     expect(model.options.step).toEqual(1);
   });
 
   it('changes "step" value if it is below 0', () => {
-    const options = { ...recievedOptions, step: -10 };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, step: -10 }, mockCoords);
 
     expect(model.options.step).toEqual(1);
   });
 
   it('changes "from" value when it is less than "min" value', () => {
-    const options = { ...recievedOptions, from: 500, double: true };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, from: -1000, min: 0 }, mockCoords);
 
     expect(model.options.from).toEqual(model.options.min);
   });
 
   it('changes "to" value when it is greater than "max" value', () => {
-    const options = { ...recievedOptions, to: 12000 };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, to: 11000, max: 10000 }, mockCoords);
 
     expect(model.options.to).toEqual(model.options.max);
   });
 
-  it('changes "to" value when it is less than "from" value', () => {
-    const options = { ...recievedOptions, from: 5000, to: 4000 };
-    const model = new Model(options);
-
-    expect(model.options.to).toEqual(model.static.from);
-  });
-
-  it('changes "from" value when it is greater than "to" value', () => {
-    const options = { ...recievedOptions, from: 5000, to: 4000 };
-    const model = new Model(options);
-
-    expect(model.options.from).toEqual(model.static.to);
-  });
-
-  it('corrects "static.from/to" value if it is undefined', () => {
-    const options = { ...recievedOptions };
-    const model = new Model(options);
-
-    expect(model.static.from).toEqual(model.options.from);
-    expect(model.static.to).toEqual(model.options.to);
-  });
-
   it('changes "from" value equal to "min" if "double" is false', () => {
-    const options = { ...recievedOptions, double: false };
-    const model = new Model(options);
+    model.updateModelOptions({ ...defaultOptions, double: false }, mockCoords);
 
     expect(model.options.from).toEqual(model.options.min);
   });
 
-  it('changes "frequancy" value if its too high or too low', () => {
-    const options = {
-      ...recievedOptions, scale: true, min: 0, max: 1000, step: 100, scaleFrequency: 20
-    };
-    const model = new Model(options);
+  it('changes "scaleFrequency" if the value is more than available steps', () => {
+    model.updateModelOptions({
+      ...defaultOptions,
+      scale: true,
+      scaleFrequency: 25,
+      min: 0,
+      max: 1000,
+      step: 100
+    }, mockCoords);
 
     expect(model.options.scaleFrequency).toEqual(11);
   });
 
-  it('triggers the "notify" function', () => {
-    const model = new Model({ ...recievedOptions });
-    jest.spyOn(model, 'updateModelOptions');
-    jest.spyOn(model.updateModelOptionsObserver, 'notify');
+  it('changes "scaleFrequency" value if the value is below 1', () => {
+    model.updateModelOptions({ ...defaultOptions, scale: true, scaleFrequency: 0 }, mockCoords);
 
-    const staticExample = { from: 5000, to: 6000 };
-    model.updateModelOptions(recievedOptions, staticExample);
-    expect(model.updateModelOptionsObserver.notify).toHaveBeenCalled();
+    expect(model.options.scaleFrequency).toEqual(1);
+  });
+
+  it('triggers the "notify" function', () => {
+    const mockFunc = jest.fn();
+
+    model.updateModelOptionsObserver.attach(mockFunc);
+    model.updateModelOptions(defaultOptions, mockCoords);
+    expect(mockFunc).toHaveBeenCalled();
   });
 });
